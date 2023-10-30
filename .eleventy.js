91678d8069367d91f98d27d00dcc06907b97f45f
kimlai <hello@kimlaitrinh.me>
2023-10-30 13:25:00 +0100

    optimize images using eleventy-img

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 const CleanCSS = require("clean-css");
 const MarkdownIt = require("markdown-it");
+const Image = require("@11ty/eleventy-img");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
@@ -13,5 +14,24 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
+  });
+
+  eleventyConfig.addShortcode("image", async function(url) {
+    const stats = await Image(url, {
+      widths: [400, 900, 1400, "auto"],
+      outputDir: "_site/img/",
+      sharpWebpOptions: { quality: 100 },
+      sharpJpegOptions: { quality: 100 },
+    });
+
+    return (`
+      <img
+        width="${stats.webp.at(-1).width}"
+        height="${stats.webp.at(-1).height}"
+        loading="lazy"
+        srcset="${stats.webp.map(s => s.srcset).join(", ")}"
+        sizes="(min-width: 120ch) 80ch"
+      />`
+    );
   });
 };
